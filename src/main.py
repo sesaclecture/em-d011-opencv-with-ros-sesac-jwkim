9e0d9111aca2ab59784dbae91d3225caa634cf53
Jinwon Kim <sesac.jwkim@gmail.com>
2026-06-19 09:02:48 +0900

    과제제출1

--- src/main.py
+++ src/main.py
@@ -21,7 +21,11 @@ def make_filter_config(
     lower,
     upper,
 ):
-    raise NotImplementedError
+    return  {
+        "color_space": color_space,
+        "lower": lower,
+        "upper": upper,
+    }
 
 
 # 문제 2.
@@ -34,7 +38,9 @@ def make_filter_config(
 #
 # (color_space, lower, upper)
 def load_filter_config(json_text):
-    raise NotImplementedError
+    
+    data = json.loads(json_text)    
+    return (data["color_space"], data["lower"], data["upper"])
 
 
 # 문제 3.
@@ -49,8 +55,12 @@ def load_filter_config(json_text):
 #
 # 객체가 없으면 None을 반환하세요.
 def find_bounding_box(mask):
-    raise NotImplementedError
-
+    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if not contours:
+        return None
+    all_contours = np.concatenate(contours)
+    x, y, w, h = cv2.boundingRect(all_contours)
+    return (x, y, w, h)
 
 # 문제 4.
 #
@@ -72,8 +82,17 @@ def decide_tracking_command(
     image_width,
     object_area,
 ):
-    raise NotImplementedError
+    if object_area >= 6000:
+        return "stop"
 
+    image_center = image_width / 2
+
+    if center_x < image_center:
+        return "turn_left"
+    elif center_x > image_center:
+        return "turn_right"
+    else:
+        return "forward"
 
 # 문제 5.
 #
@@ -83,4 +102,13 @@ def decide_tracking_command(
 #
 # (linear_x, angular_z)
 def command_to_twist(command):
-    raise NotImplementedError
+    if command == "turn_left":
+        return (0.0, 0.5)
+    elif command == "turn_right":
+        return (0.0, -0.5)
+    elif command == "forward":
+        return (0.2, 0.0)
+    elif command == "stop":
+        return (0.0, 0.0)
+    elif command == "invalid":
+        return (0.0, 0.0)
